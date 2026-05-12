ddb556222e24fd8506300baecdcce3319c4fcbec
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-05-12 15:08:27 +0800

    comments with number sign

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-print("Hello from Python")
+print("Hello from Python") # Code starting line mark
 
-x = 2
-y = 1
+x = 2 # x variable with value of 2
+y = 1 # y variable with value of 1
 
 if x > y:
     print("2 the value of x is greater than y which has the value 1")
 
-print("End of code...")
+print("End of code...") # Code finished mark
