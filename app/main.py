ab2702a58e4900d22f9fa38257edec2939dee64d
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-05-15 12:02:55 +0800

    casting variables and printing variable types

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 print("Hello from Python") # Code starting line mark
 
-x = 2 # x variable with value of 2
-y = 1 # y variable with value of 1
+x = int(2) # x variable with value of 2
+y = int(1) # y variable with value of 1
+z = str("2 the value of x is greater than y which has the value 1")
 
 if x > y:
-    print("2 the value of x is greater than y which has the value 1")
+    print(z)
 
-print("End of code...") # Code finished mark
+print(type(x))
+print(type(y))
+print(type(z))
+
+print("End of code...") # Code finished markg
